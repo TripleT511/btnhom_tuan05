3a12f7846ca67784ef66731f55bb436f3bb96120
TripleT511 <0306191074@caothang.edu.vn>
2021-11-04 11:32:01 +0700

    Update login page

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,103 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPagePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LoginPagePageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    Widget _btnLogin = Container(
+      margin: const EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 0),
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.black,
+            padding: const EdgeInsets.all(10.0),
+            minimumSize: const Size(500, 60),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.zero))),
+        child: const Text(
+          "SIGN IN",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
+    );
+    Widget _logo = Container(
+      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+      child: Image.asset(
+        'images/1.png',
+        width: 180,
+        height: 180,
+        fit: BoxFit.cover,
+      ),
+    );
+    Widget _title = Container(
+      padding: const EdgeInsets.only(top: 10.0, bottom: 5.0),
+      child: const Text(
+        'SignIn',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 40.0,
+        ),
+      ),
+    );
+    Widget _title2 = Container(
+      margin: const EdgeInsets.only(bottom: 40),
+      child: const Text(
+        'Speak, friend and enter',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
+      ),
+    );
+    Widget _inputSection = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(15.0),
+          child: const TextField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: InputBorder.none,
+              hintText: 'Password',
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(15.0),
+          child: const TextField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: InputBorder.none,
+              hintText: 'Email',
+            ),
+          ),
+        ),
+      ],
+    );
+    return Scaffold(
+      backgroundColor: Colors.blue.shade500,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[_logo, _title, _title2, _inputSection, _btnLogin],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
