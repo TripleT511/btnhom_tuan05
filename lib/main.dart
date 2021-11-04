1ae4866252fd30d33a7bd1b1fa33aab1d4914f65
PhucSmRePo <0306191060@caothang.edu.vn>
2021-11-04 13:26:02 +0700

    updatemailpage

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const MailPage(),
     );
   }
 }
@@ -116,5 +116,90 @@ class _LoginPagePageState extends State<LoginPage> {
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MailPage extends StatelessWidget {
+  const MailPage({Key? key}) : super(key: key);
+
+  Container buildF(IconData i, double pad, String str) {
+    bool value1 = false;
+    bool value2 = false;
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(bottom: 15),
+      child: Row(
+        children: [
+          Checkbox(
+            value: value1,
+            onChanged: (value2) {
+              value1 = true;
+            },
+          ),
+          Icon(
+            i,
+            color: Colors.blueAccent,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10, bottom: 10),
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom:
+                        BorderSide(color: Colors.grey.shade200, width: 1.0))),
+            child: Text(
+              str,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: pad, bottom: 10),
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom:
+                        BorderSide(color: Colors.grey.shade200, width: 1.0))),
+            child: Text(
+              '1',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool value1 = false;
+    bool value2 = false;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Mail'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 20, top: 20, bottom: 10),
+                  color: Colors.grey.shade200,
+                  child: Text('Mailboxes'),
+                ),
+                buildF(Icons.inbox, 300, 'All Inboxes'),
+                buildF(Icons.cloud, 328, 'ICloud'),
+                buildF(Icons.mail_outline, 330, 'Gmail'),
+                buildF(Icons.mail, 315, 'Hot Mail'),
+                buildF(Icons.star, 345, 'Vip'),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 20, top: 20, bottom: 10),
+                  color: Colors.grey.shade200,
+                  child: Text('Special folder'),
+                ),
+                buildF(Icons.security, 317, 'Sercure'),
+                buildF(Icons.notification_add, 292, 'Nofications'),
+              ],
+            ),
+          ),
+        ));
   }
 }
